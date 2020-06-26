c36d1a57938b787057826e56b73fa9695f189596
leonliudb <dl2868@nyu.edu>
2020-06-26 02:45:56 -0700

    Setup

---
+++ app.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+Symbol = 'AMZN TSLA'
+
+StockData = yf.Tickers(Symbol)
+
+StockPrices = StockData.history(period='1d', start='2020-1-1', end='2020-1-10')
+
+print(StockPrices)
+
+
+
