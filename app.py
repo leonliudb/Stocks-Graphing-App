5d7500c59f16ef4fb31cca24c94d6aad4a24bb16
leonliudb <dl2868@nyu.edu>
2020-06-26 03:14:18 -0700

    Algorithm

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ Symbol = 'AMZN TSLA'
 
 StockData = yf.Tickers(Symbol)
 
-StockPrices = StockData.history(period='1d', start='2020-1-1', end='2020-1-10')
+StockPrices = StockData.history(period='1d', start='2017-1-1', end='2020-2-10')
 
-print(StockPrices)
+#print(StockPrices)
+#print(StockPrices['Close'].head())
+ClosePrices = StockPrices['Close']
+stock_return = ClosePrices.apply(lambda x: x / x[0])
+#print(stock_return.head())
 
 
-
-StockPrices['Close'].plot(grid=True)
-
+#StockPrices['Close'].plot(grid=True)
+stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
 plt.show()
