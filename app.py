01a57fd683b2d2bc28e4594e8283df16ce937997
leonliudb <dl2868@nyu.edu>
2020-06-28 02:28:45 -0700

    error handling 1

--- app.py
+++ app.py
@@ -5,20 +5,36 @@ import pandas as pd
 
 
 #Symbol = 'SPY AMZN TSLA'
-selected_symbol = input("Please input stock symbols, i.e. 'SPY AMZN TSLA' : " )
+while True:
+    selected_symbol = input("Please input two or more stock symbols, i.e. 'SPY AMZN TSLA' : " )
+    try:
+        StockData = yf.Tickers(selected_symbol)
+        sp = StockData.history()
+        break
+    except KeyError:
+        print("Please enter valid symbols")
+        
+
 
 
 #StockData = yf.Tickers(Symbol)
-StockData = yf.Tickers(selected_symbol)
+
 #today = datetime.datetime.today().isoformat()[:10]
 
 while True:
     startdate = input("Please enter the start date in this format '2001-1-1' : " )
     enddate = input("Please enter the end date in this format '2001-1-1' : " )
-    StockPrices = StockData.history(period='1d', start=startdate, end=enddate)
-    ClosePrices = StockPrices['Close']
+    try:
+        StockPrices = StockData.history(period='1d', start=startdate, end=enddate)
+        ClosePrices = StockPrices['Close']
+    except ValueError:
+        print("Wrong format")
     if str(ClosePrices.isnull().values.any()) == "True":
         print("Sorry, please enter a proper time range for the selected stock symbols")
+    elif not startdate:
+        print("no input start date")
+    elif not enddate:
+        print("no input end date")   
     else:
         break
 
@@ -35,12 +51,9 @@ stock_return = ClosePrices.apply(lambda x: x / x[0])
 #print(stock_return.head())
 
 
-
-#StockPrices['Close'].plot(grid=True)
 stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
 
 
 #plt.show(block=False)
-
 
 plt.show()
