93c9362a991020e7687161ad10a2e9c69f81a461
leonliudb <dl2868@nyu.edu>
2020-06-28 04:09:54 -0700

    exception handling 2

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ while True:
         break
     except KeyError:
         print("Please enter valid symbols")
+    except ValueError:
+        print("Please input stock symbols")
         
 
 
@@ -23,20 +25,27 @@ while True:
 
 while True:
     startdate = input("Please enter the start date in this format '2001-1-1' : " )
+    if not startdate:
+        print("no input start date")
     enddate = input("Please enter the end date in this format '2001-1-1' : " )
+    if not enddate:
+        print("no input end date")
     try:
-        StockPrices = StockData.history(period='1d', start=startdate, end=enddate)
-        ClosePrices = StockPrices['Close']
+        datetime.datetime.strptime(startdate, "%Y-%m-%d")
+        datetime.datetime.strptime(enddate, "%Y-%m-%d")
     except ValueError:
         print("Wrong format")
-    if str(ClosePrices.isnull().values.any()) == "True":
-        print("Sorry, please enter a proper time range for the selected stock symbols")
-    elif not startdate:
-        print("no input start date")
-    elif not enddate:
-        print("no input end date")   
     else:
-        break
+        StockPrices = StockData.history(period='1d', start=startdate, end=enddate)
+        ClosePrices = StockPrices['Close']
+        if str(ClosePrices.isnull().values.any()) == "True":
+            print("Sorry, please enter a proper time range for the selected stock symbols")
+        elif not startdate:
+            print("no input start date")
+        elif not enddate:
+            print("no input end date")   
+        else:
+            break
 
 #StockPrices = StockData.history(period='1d', start='2010-1-1', end='2020-2-10')
 #StockPrices = StockData.history(period='1d', start=startdate, end=today)
@@ -46,14 +55,16 @@ while True:
 
 
 
+ClosePrices.plot()
+plt.title("Stocks Historical Prices(Daily)")
+
 
 stock_return = ClosePrices.apply(lambda x: x / x[0])
 #print(stock_return.head())
 
-
 stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
+plt.title("Stocks Return Comparison")
 
 
 #plt.show(block=False)
-
-plt.show()
+#plt.show()
