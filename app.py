cf1148ac3389ba7dd0e5ee055ea63971e591e206
leonliudb <dl2868@nyu.edu>
2020-06-28 22:30:59 -0700

    end date limit

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from yfinance import multi, shared
 
 
 print("-------------------------")
+today = datetime.datetime.today().isoformat()[:10]
+print(today)
+
 #Symbol = 'SPY AMZN TSLA'
 while True:
     selected_symbol = input("Please input two or more stock symbols. Each symbol must be separated by one single space, e.g. 'SPY AMZN TSLA': " )
@@ -20,15 +23,12 @@ while True:
         print("Please enter at least two valid stock symbols")
     except ValueError:
         print("Please input some stock symbols")
-    
-   
-    
+       
 
 
-#today = datetime.datetime.today().isoformat()[:10]
 
 while True:
-    startdate = input("Please enter the start date in this format '2001-1-1' : " )
+    startdate = input("Please enter the start date in this format '2001-01-01': " )
     if not startdate:
         print("no input start date")
     else:
@@ -38,16 +38,18 @@ while True:
             print("Incorrect start date format")
         else:
             while True:
-                enddate = input("Please enter the end date in this format '2001-1-1' : " )
+                enddate = input("Please enter the end date in this format '2001-01-01': " )
                 if not enddate:
                     print("no input end date")
                 else:
                     try:
                         datetime.datetime.strptime(enddate, "%Y-%m-%d")
+                        if enddate > today:
+                            print("The end date must be set ealier than or on Today's date: ",today)
+                        else:
+                            break
                     except ValueError:
-                        print("Incorrect end date format")
-                    else:
-                        break
+                        print("Incorrect end date format")                        
             StockPrices = StockData.history(period='1d', start=startdate, end=enddate)
             ClosePrices = StockPrices['Close']
             if str(ClosePrices.isnull().values.any()) == "True":#check if there's any NaN value which means the stock price is not available at this time/date
@@ -61,34 +63,31 @@ while True:
 #print(type(StockPrices['Close'].head()))
 
 
-
-
-ClosePrices.plot()
+ClosePrices.plot(grid = True)
 plt.title("Stocks Historical Prices(Daily)")
+plt.savefig('graphs/Stocks Historical Prices.png')
 
 
 stock_return = ClosePrices.apply(lambda x: x / x[0])
 #print(type(stock_return.tail(1)))
+
+stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
+plt.title("Stocks Return Comparison")
+plt.savefig('graphs/Stocks Return Comparison.png')
+
+
 last_return = stock_return.tail(1)
-#print(last_return.idxmax(axis = 1))
-#print(type(last_return.idxmax(axis = 1)))
-#print(last_return.idxmax(axis = 1).values)
-print(last_return.T)
+#print(last_return.T)
 ranking = last_return.T.rank(ascending = False).astype(int)
 #print(ranking)
 print(ranking.to_string(header=False))
 #print(type(ranking.to_string(header=False))) #str
 
-#print(type(ranking))
-
-
-
-stock_return.plot(grid = True).axhline(y = 1, color = "black", lw = 2)
-plt.title("Stocks Return Comparison")
 
 
 #plt.show(block=False)
-#plt.show()
+plt.show()
+
 
 
 
